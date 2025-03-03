0b48795bc226a6fb63f6f7c02e9a4578d1d81ca2
Deniz <candenizozder08@gmail.com>
2025-03-03 03:14:25 +0300

    Tasks Done

---
+++ js/task-2.js
@@ -0,0 +1,17 @@
+console.log("JS-2 -----------------------------------");
+function makeArray(firstArray, secondArray, maxLength)
+{
+    const dizi = firstArray.concat(secondArray);
+    if ( dizi.length > maxLength)
+    {
+        return dizi.slice(0,maxLength);
+    }
+    return dizi;
+}
+
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
---
+++ js/task-3.js
@@ -0,0 +1,15 @@
+console.log("JS-3 -----------------------------------");
+function filterArray(numbers, value){
+    const temp = [];
+    for (const number of numbers){
+        if (number > value){
+            temp.push(number);
+        }
+    }
+    return temp;
+}
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
